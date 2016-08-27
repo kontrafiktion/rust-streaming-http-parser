beadeefbc6ff0ce158a80556cd5e7b68d0c8765e
Travis CI <noreply@travis-ci.com>
2016-08-27 15:56:15 +0000

    Update documentation

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["libc"] = [];implementors["http_muncher"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html' title='core::fmt::Debug'>Debug</a> for <a class='struct' href='http_muncher/struct.Parser.html' title='http_muncher::Parser'>Parser</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
